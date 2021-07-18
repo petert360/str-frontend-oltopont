497c9557cd099c50a63929638f621b56ba2f7613
petert360 <peter.t360@hotmail.com>
2021-07-18 16:34:35 +0200

    Test: patients adatok lekérése

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patient } from './model/patient';
+import { PatientService } from "./service/patient.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  patients: Observable<Patient[]> = this.patientService.getAll();
+
+  constructor(
+    private patientService: PatientService,
+  ) { }
 }
