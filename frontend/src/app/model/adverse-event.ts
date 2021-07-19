01117ada282a894c19a328b589e16c6c21509244
petert360 <73585032+petert360@users.noreply.github.com>
2021-07-19 13:04:29 +0200

    Update adverse-event.ts

--- frontend/src/app/model/adverse-event.ts
+++ frontend/src/app/model/adverse-event.ts
@@ -1,6 +1,7 @@
 import { Patient } from "./patient";
 
 export class AdverseEvent {
+    _id: string = '';
     patient: Patient = new Patient;
     reported: Date = new Date(); // Date reported
     description: string = ''; // Description of the adverse event
