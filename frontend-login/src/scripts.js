e771ae424567ad90c8e7f4020b9e8cf2627629e9
petert360 <peter.t360@hotmail.com>
2021-08-02 01:27:37 +0200

    Remove chartist

--- frontend-login/src/scripts.js
+++
@@ -1,9 +0,0 @@
-new Chartist.Line('#traffic-chart', {
-    labels: ['January', 'Februrary', 'March', 'April', 'May', 'June'],
-    series: [
-      [23000, 25000, 19000, 34000, 56000, 64000]
-    ]
-  }, {
-  low: 0,
-  showArea: true
-});
