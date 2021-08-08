6d04a91e5ae5314475c9ac899900ea24790d9cf9
petert360 <peter.t360@hotmail.com>
2021-08-09 00:42:44 +0200

    Vaccine mongoose schema

--- backend/src/models/vaccine.model.js
+++ backend/src/models/vaccine.model.js
@@ -1,9 +1,29 @@
 const mongoose = require(mongoose);
-const Schema = mongoose.Schema;
 
-const VaccineSchema = Schema({
+const VaccineSchema = mongoose.Schema(
+    {
+/* _id-ra nincs szükség, mert a mongoDB automatikusan kezeli
     _id: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
+    }, */
+        manufacture: String,
+        product: String,
+        lot: String,
+        doseInterval: Number,
     },
-    
-});
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.module('Vaccine', VaccineSchema);
+
+/*
+export class Vaccine {
+    _id: string = '';
+    manufacture: string = '';
+    product: string = '';
+    lot: string = '';
+    doseInterval: number = 0; // Days between first and second doses
+}
+*/
