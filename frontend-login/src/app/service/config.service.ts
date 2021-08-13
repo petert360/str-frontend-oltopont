cb454260429dc6f06b3b2578d060fb76a1c4b780
petert360 <73585032+petert360@users.noreply.github.com>
2021-08-13 02:17:09 +0200

    config, appointment

--- frontend-login/src/app/service/config.service.ts
+++ frontend-login/src/app/service/config.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
 
+export interface ITableCol {
+  key: string,
+  text: string,
+  editable?: boolean,
+  checkbox?: boolean,
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
   apiUrl = 'http://localhost:3000/';
+
+  patientColumns: ITableCol[] = [
+    { key: 'olt_idopont', text: 'Idő' },
+    { key: 'pt_nev', text: 'Név' },
+    { key: 'pt_taj', text: 'TAJ' },
+    { key: 'vaccine', text: 'Vakcina' },
+    { key: 'megjelent', text: 'Kész', checkbox: true },
+  ]
+
 
   constructor() { }
 }
