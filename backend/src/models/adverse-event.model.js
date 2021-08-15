828feb15f42fda4a0a5a4241079e260a77a435fd
petert360 <peter.t360@hotmail.com>
2021-08-15 09:37:37 +0200

    update

--- backend/src/models/adverse-event.model.js
+++ backend/src/models/adverse-event.model.js
@@ -26,15 +26,3 @@ const AdverseEventSchema = mongoose.Schema(
 );
 
 module.exports = mongoose.model('AdverseEvent', AdverseEventSchema);
-
-/*
-import { Patient } from "./patient";
-
-export class AdverseEvent {
-    _id: string = '';
-    patient: Patient = new Patient;
-    date: Date = new Date(); // Date reported
-    description: string = ''; // Description of the adverse event
-}
-
-*/
