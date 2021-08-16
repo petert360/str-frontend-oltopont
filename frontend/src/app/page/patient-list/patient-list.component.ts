8593a42e3d7a7457ef331589e4abe791a5ad27bd
petert360 <peter.t360@hotmail.com>
2021-08-16 23:29:45 +0200

    Paginator funció hozzáadása a patient listához

--- frontend/src/app/page/patient-list/patient-list.component.ts
+++ frontend/src/app/page/patient-list/patient-list.component.ts
@@ -5,6 +5,10 @@ import { Patient } from 'src/app/model/patient';
 import { ConfigService, ITableCol } from 'src/app/service/config.service';
 import { PatientService } from 'src/app/service/patient.service';
 
+interface IPageBtn {
+  page: number;
+}
+
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
@@ -22,13 +26,26 @@ export class PatientListComponent implements OnInit {
 
   phrase: string = "";
 
+  // Paginator változók
+  patientAll: Patient[] = [];
+  patientNum: number = 0;
+  pageSize: number = 5;
+  pageStart: number = 1;
+  currentPage: number = 1;
+
+
   constructor(
     private patientService: PatientService,
     private config: ConfigService,
   ) { }
 
   ngOnInit(): void {
-    console.log(this.list$)
+    // console.log(this.list$)
+    this.list$.subscribe(data => {
+      this.patientAll = data;
+      this.patientNum = data.length;
+    })
+
   }
 
   // Osztop fejlécére kattintás
@@ -40,8 +57,11 @@ export class PatientListComponent implements OnInit {
   // Keresőkifejezés változása
   onChangePhrase(event: Event): void {
     this.phrase = (event.target as HTMLInputElement).value;
-  }
+    this.patientNum = this.patientAll.filter(item =>
+      item.name.toLowerCase().includes(this.phrase.toLowerCase())).length
+    this.currentPage = 1;
 
+  }
 
   onDelete(patient: Patient): void {
     if (confirm("A törlés megerősítéséhez nyomja meg az OK gombot")) {
@@ -51,6 +71,37 @@ export class PatientListComponent implements OnInit {
     } else {
       console.log('Delete operation cancelled')
     }
+  }
+
+  // Paginator
+  get paginator(): IPageBtn[] {
+    const pages: IPageBtn[] = [];
+    for (let i = 0; i < this.patientNum / this.pageSize && pages.length < 10; i++) {
+      const page = this.pageStart + i;
+      pages.push({ page });
+    }
+    return pages;
+  }
+
+  get pageSliceStart(): number {
+    const index = this.currentPage - 1;
+    return index === 0 ? 0 : (index * this.pageSize);
+  }
+
+  get pageSliceEnd(): number {
+    return this.pageSliceStart + this.pageSize;
+  }
+
+  onPaginate(event: Event, btn: IPageBtn): void {
+    event.preventDefault();
+    this.currentPage = btn.page;
+    this.pageStart = (btn.page - 5) < 1 ? 1 : (btn.page - 5);
+  }
+
+  onStepPage(event: Event, step: number): void {
+    event.preventDefault();
+    this.currentPage += step;
+    this.pageStart = (this.currentPage - 5) < 1 ? 1 : (this.currentPage - 5);
   }
 
 }
