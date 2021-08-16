efa08a741e99c8744d19aad00ae4771ad381504a
petert360 <peter.t360@hotmail.com>
2021-08-16 21:28:39 +0200

    példa route-ok kikapcsolása

--- backend/src/server.js
+++ backend/src/server.js
@@ -73,9 +73,9 @@ app.post('/logout', authHandler.logout);
 // Példa route-ok
 // Beékelhetünk több middleware-t, előbb az authenticatejwt, ami az authenticate.js-ben szereplően vizsgálaja az authorizációt és a usert elhelyezi a kérésben.
 // A /person url-t az érheti el, aki be van jelentkezve
-app.use('/person', authenticateJwt, require('./controllers/person/person.routes'));
+// app.use('/person', authenticateJwt, require('./controllers/person/person.routes'));
 // A /post url-t az érheti el, aki be van jelentkezve és admin
-app.use('/post', authenticateJwt, adminOnly, require('./controllers/post/post.routes'));
+// app.use('/post', authenticateJwt, adminOnly, require('./controllers/post/post.routes'));
 
 // Swagger route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
